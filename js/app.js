dc1a0aa222aa6b3453ba439388f5b439d519d3bf
gallasmur <jrabal.segura@gmail.com>
2018-04-05 11:47:06 +0200

    Add esqueleton por player class

--- js/app.js
+++ js/app.js
@@ -21,28 +21,43 @@ class Enemy extends Entity {
     }
 }
 
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Player extends Entity {
+    constructor(posX, posY, speed) {
+        super(posX, posY, speed);
+        this.sprite = "images/char-boy.png";
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    update() {
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    handleInput() {
+
+    }
+}
+
+// // Enemies our player must avoid
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
+
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
+
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
+
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
 // Now write your own player class
 // This class requires an update(), render() and
